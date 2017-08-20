8c4fead2c44bbb19d6800c645f257a7c70be24be
Вяткин Никита Алексеевич <navyatkin@edu.hse.ru>
2017-08-20 19:08:42 +0500

    Готовый вариант программы. Версия 1.0

--- Tenth/Tenth/MyList.cs
+++ Tenth/Tenth/MyList.cs
@@ -7,155 +7,69 @@ using System.Threading.Tasks;
 namespace Tenth
 {
 
-        class MyList //Класс для однонаправленного списка
+    class MyList //Класс для однонаправленного списка
+    {
+        private double data;//иформационное поле элемента списка
+        private MyList next;//адресное поле элемента списка (указывает на следующий элемент)
+        public double Data
         {
-            public int data;//иформационное поле элемента списка (int)
-            public MyList next;//адресное поле элемента списка (указывает на следующий элемент)
-            public MyList()
+            get { return data; }
+            set { data = value; }
+        }//Свойство data
+        public MyList Next
+        {
+            get { return next; }
+            set { next = value; }
+        }//Свойство next
+        public MyList()//Создание пустого элемента списка
+        {
+            data = 0;
+            next = null;
+        }
+        public MyList(double d)//Создание элемента списка
+        {
+            data = d;
+            next = null;
+        }
+        public void Add(double info)//Добавление элемента с указанным информационным полем в конец списка
+        {
+            MyList end = this;
+            while (end.next != null)
             {
-                data = 0;
-                next = null;
-            }//Создание пустого элемента  списка
-            public MyList(int d)
-            {
-                data = d;
-                next = null;
-            }//Создание элемента списка
-            public override string ToString()
-            {
-                return data + " ";
+                end = end.next;
             }
-            public static MyList MakeElem(int d)
+            end.next = new MyList(info);
+          
+        }
+        public void ShowList()//Показать список
+        {
+            if (this == null)
             {
-                MyList p = new MyList(d);
-                return p;
-            }//Создание элемента списка
-            public static MyList DobNach(int size)
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            MyList p = this;
+            while (p != null)
             {
-                Random rnd = new Random();
-                int info = rnd.Next(11);
-                Console.WriteLine("Добавление числа {0}...", info);
-                MyList beg = MakeElem(info);
-                for (int i = 1; i < size; i++)
-                {
-                    info = rnd.Next(11);
-                    Console.WriteLine("Добавление числа {0}...", info);
-                    MyList p = MakeElem(info);
-                    p.next = beg;
-                    beg = p;
-                }
-                return beg;
-            }//Создание списка добавлением элементов в начало списка
-            public static MyList DobKon(int size)
+                Console.Write(p + " ");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+        public double FindEnd()//Нахождение информационного поля последнего элемента списка
+        {
+            MyList end = this;
+            while (end.next != null)
             {
-                Random rnd = new Random();
-                int info = rnd.Next(11);
-                Console.WriteLine("Добавление числа {0}...", info);
-                MyList beg = MakeElem(info);
-                MyList r = beg;
-                for (int i = 1; i < size; i++)
-                {
-                    info = rnd.Next(11);
-                    Console.WriteLine("Добавление числа {0}...", info);
-                    MyList p = MakeElem(info);
-                    r.next = p;
-                    r = p;
-                }
-                return beg;
-            }//Создание списка добавлением элементов в конец списка
-            public static void ShowList(MyList beg)
-            {
-                if (beg == null)
-                {
-                    Console.WriteLine("Список пуст");
-                    return;
-                }
-                MyList p = beg;
-                while (p != null)
-                {
-                    Console.Write(p + " ");
-                    p = p.next;
+                end = end.next;
+            }
+            return end.Data;
+        }
+        public override string ToString()
+        {
+            return data + " ";
+        }
 
-                }
-                Console.WriteLine();
-            }//Показать список
-            public static MyList Dobavlenie(MyList beg, int number)
-            {
-                Random rnd = new Random();
-                int info = rnd.Next(11);
-                Console.WriteLine("Добавление числа {0}...", info);
-                MyList NewElem = MakeElem(info);
-                if (beg == null)
-                {
-                    beg = MakeElem(rnd.Next(11));
-                    return beg;
-                }
-                if (number == 1)
-                {
-                    NewElem.next = beg;
-                    beg = NewElem;
-                    return beg;
-                }
-                MyList p = beg;
-                for (int i = 1; i < number - 1 && p != null; i++)
-                    p = p.next;
-                if (p == null)
-                {
-                    Console.WriteLine("Ошибка! Длина списка меньше введённого номера");
-                    return beg;
-                }
-                NewElem.next = p.next;
-                p.next = NewElem;
-                return beg;
-            }//Добавление элемента в список на указанное место
-            public static MyList Ydalenie(MyList beg, int number)
-            {
-                if (beg == null)
-                {
-                    return null;
-                }
-                if (number == 1)
-                {
-                    beg = beg.next;
-                    return beg;
-                }
-                MyList p = beg;
-                for (int i = 1; i < number - 1 && p != null; i++)
-                    p = p.next;
-                if (p == null)
-                {
-                    Console.WriteLine("Размер списка меньше введённого числа");
-                    return beg;
-                }
-                p.next = p.next.next;
-                return beg;
-            }//Удаление элемента списка с указанным номером
-            public static MyList Z1YdaleniePoslChetNum(MyList beg)
-            {
-                MyList p = beg;
-                int count = 1, number = 0;
-                if (beg == null)
-                {
-                    Console.WriteLine("Список пуст");
-                    return beg;
-                }
-                do
-                {
-                    if (p.data % 2 == 0)
-                        number = count;
-                    p = p.next;
-                    count++;
-                } while (p != null);
-                if (number == 0)
-                {
-                    Console.WriteLine("Нет элементов с чётным информационным полем");
-                    return beg;
-                }
-                beg = Ydalenie(beg, number);
 
-                return beg;
-            }//Удаление последнего элемента списка с чётным информационным полем
-
-        
     }
 }
--- Tenth/Tenth/Program.cs
+++ Tenth/Tenth/Program.cs
@@ -8,8 +8,63 @@ namespace Tenth
 {
     class Program
     {
+        static MyList beg;//Певрый элемент вводимой последовательности
+        static int n;//Число n
+        static int VvodProverka(int mogr = 0, int bogr = 0)//Проверка вводимых с клавиатуры чисел (целых), mogr и bogr - минимально и максимально возможные значения числа
+        {
+            bool ok;
+            int n;
+            do
+            {
+                ok = int.TryParse(Console.ReadLine(), out n);
+                if (!ok) { Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод."); continue; }
+                if ((n < mogr) && (bogr < mogr)) { Console.WriteLine("Ошибка. Число должно быть больше или равно {0} . Повторите ввод.", mogr); ok = false; continue; }
+                if ((n < mogr) && (mogr != 0) || ((n > bogr) && (bogr != 0))) { Console.WriteLine("Ошибка. Число должно находится в пределах от {0} до {1} . Повторите ввод.", mogr, bogr); ok = false; }
+
+            } while (!ok);
+            Console.WriteLine();
+            return n;
+        }
+        static double VvodProverka()//Проверка действительных чисел
+        {
+            bool ok;
+            double n;
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out n);
+                if (!ok) { Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод.");}
+            } while (!ok);
+            Console.WriteLine();
+            return n;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число n");
+            n = VvodProverka(2);
+
+            Console.WriteLine("Введите 1-й элемент");
+            beg = new MyList(VvodProverka());//Добавление первого элемента последовательности
+            for (int i = 2; i <= n; i++)//Добавление остальных элементов последовательности
+            {
+                Console.WriteLine("Введите {0}-й элемент", i);
+                beg.Add(VvodProverka());
+            }
+
+            Console.Clear();
+            Console.WriteLine("Введённая последовательность");
+            beg.ShowList();//Вывод в консоль введённой последовательности
+
+            double ListEnd = beg.FindEnd();//Выевление последнего элемента последовательности
+
+            MyList Slide = beg;//По порядку принимает значение всех элементов последовательности (кроме последнего)
+            Console.WriteLine("Искомая последовательность");
+            while (Slide.Next != null)//Вычисление и вывод на экран искомой последовательности
+            {
+                double t = Slide.Data - ListEnd;//Значение текущего элемента минус значение последнего
+                Console.Write(t + " ");
+                Slide = Slide.Next;
+            }
+            Console.ReadLine();
         }
     }
 }
